9a603b2f87aea3e0ae27fbe965728dea17877fe0
Jennifer Giri <jenniferanngiri@gmail.com>
2022-02-09 19:08:31 -0500

    wrote requests/requests handlers for products and cart

--- server/apiconnect.js
+++ server/apiconnect.js
@@ -1,7 +1,21 @@
 const axios = require('axios');
 const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe';
-//import token from config
+const TOKEN = require('../config.js')
 
+
+//get list of all products
+const getAllProducts = () => {
+  let options = {
+    url: `${url}/products`,
+    headers: {
+      'Authorization': `token ${config.TOKEN}`
+      'Content-Type': 'application/json'
+    }
+  };
+  return axios.get(options)
+}
+
+//get data for product with this ID
 const getProductInfo = (prodID) => {
   //options?
   let options = {
@@ -14,17 +28,41 @@ const getProductInfo = (prodID) => {
   return axios.get(options)
 }
 
+//gets all styles of product with this id
+const getAllStyles = (prodID) => {
+  //options?
+  let options = {
+    url: `${url}/products/${prodID}/styles`,
+    headers: {
+      'Authorization': `token ${config.TOKEN}`
+      'Content-Type': 'application/json'
+    }
+  };
+  return axios.get(options)
+}
 
-//this is a test line for the git workflow let's seeeee!!!!!
+//retrieves list of products added to card
+const getItemsInCart = () => {
+  let options = {
+    url: `${url}/cart`,
+    headers: {
+      'Authorization': `token ${config.TOKEN}`
+      'Content-Type': 'application/json'
+    }
+  };
+  return axios.get(options)
+}
 
-
-
-
-
-
-
-module.exports = {
-  getProductInfo: getProductInfo
+//adds a product to the cart
+const addToCart = (prodID) => {
+  let options = {
+    url: `${url}/cart/`,
+    headers: {
+      'Authorization': `token ${config.TOKEN}`
+      'Content-Type': 'application/json'
+    }
+  };
+  return axios.post(options, prodID)
 }
 
 
@@ -34,12 +72,10 @@ module.exports = {
 
 
 
-
-
-
-
-
-
-
-
-
+module.exports = {
+  getAllProducts: getAllProducts,
+  getProductInfo: getProductInfo,
+  addToCart: addToCart,
+  getItemsInCart: getItemsInCart,
+  getAllStyles: getAllStyles
+}
