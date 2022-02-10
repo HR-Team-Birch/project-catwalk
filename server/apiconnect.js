2ba5d7a3848e3c5ead61ac9449577faf842cfce2
BJ White <bjwhite0512@gmail.com>
2022-02-09 19:54:57 -0500

    test

--- server/apiconnect.js
+++ server/apiconnect.js
@@ -1,17 +1,28 @@
 const axios = require('axios');
 const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe';
+const token = require('../config.js');
+
 //import token from config
 
+let options = {
+  url: `${url}/products/${prodID}`,
+  headers: {
+    'Authorization': `token ${config.TOKEN}`
+    'Content-Type': 'application/json'
+  }
+};
 const getProductInfo = (prodID) => {
   //options?
-  let options = {
-    url: `${url}/products/${prodID}`,
-    headers: {
-      'Authorization': `token ${config.TOKEN}`
-      'Content-Type': 'application/json'
-    }
-  };
   return axios.get(options)
+}
+
+const getQuestions = (prodId) => {
+  axios.get({
+    url : `${url}`
+    })
+
+
+
 }
 
 
