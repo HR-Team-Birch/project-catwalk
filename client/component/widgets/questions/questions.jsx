54c9b99b70544b389f7e4791fbeed28591fb6e7a
BJ White <bjwhite0512@gmail.com>
2022-02-14 17:09:16 -0500

    questions react frame

--- client/component/widgets/questions/questions.jsx
+++ client/component/widgets/questions/questions.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import SearchQuestions from './components/searchQuestions.jsx';
 import QuestionList from './components/questionList.jsx';
+const axios = require('axios');
+const url = 'http://localhost:3000/qa/questions/?product_id=';
 
 
-class Questions extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      curProduct: {}
-    }
-  }
+const Questions = ({productId}) => {
+  const [productQuestions, setProductQuestions] = useState ([]);
+  //console.log(productId);
 
-render() {
+  const fetchQuestions = (productId) => {
+    axios.get(`${url}${productId}`)
+    .then((result) => {
+      //console.log('result data', result.data);
+      setProductQuestions(result.data.results);
+    })
+    .catch((error) => {
+      console.log('error: ', error);
+    });
+  };
+
+  useEffect(() => {
+    productId ? fetchQuestions() : null;
+  }, [productId]);
+
   return(
     <div className="questions">
       <h1>Questions</h1>
-      <SearchQuestions/>
-      <QuestionList/>
+      <SearchQuestions fetchQuestions={fetchQuestions}/>
+      <QuestionList productQuestions={productQuestions}/>
     </div>
   )
-}
 
 }
 
