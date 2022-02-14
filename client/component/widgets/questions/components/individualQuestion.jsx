1b1534e0b1b809313eadd5c44729887519123339
BJ White <bjwhite0512@gmail.com>
2022-02-14 17:22:04 -0500

    questions react widget

---
+++ client/component/widgets/questions/components/individualQuestion.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const IndividualQuestion = ({question}) => {
+
+  console.log('question in individual question', question);
+
+
+
+
+  return (
+    <div className="eachQuestion">
+      <div id="question">Q: {question.question_body}</div>
+      <a href="" id="addAnswer">Add Answer</a>
+      <div>A: Well if you don't use one right now you are going to learn so much more</div>
+    </div>
+  )
+}
+
+export default IndividualQuestion;
---
+++ client/component/widgets/questions/components/searchQuestions.jsx
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from 'react';
+
+const SearchQuestions = ({fetchQuestions}) => {
+
+  const [searchTerm, setSearchTerm] = useState ('');
+
+  const searchBarChange = (event) => {
+    setSearchTerm(event.target.value);
+  }
+
+  const submitSearch = () => {
+    console.log('submitting search for', searchTerm);
+    fetchQuestions(searchTerm);
+  }
+
+  return (
+    <div id="searchQuestions">
+      <input id="searchBar" type="search" size="122" placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..." onChange={searchBarChange}></input>
+      <button onClick={submitSearch}>Submit</button>
+    </div>
+  )
+}
+
+export default SearchQuestions;
