6aefd9713a2e403b6df9c9ac8af890f2387611c1
BJ White <bjwhite0512@gmail.com>
2022-02-14 17:21:38 -0500

    related component

--- client/component/widgets/related/related.jsx
+++ client/component/widgets/related/related.jsx
@@ -1,37 +1,16 @@
 import React from 'react'
-const axios = require('axios');
-const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe';
-// const TOKEN = require('...../config.js')
-const express = require('express');
-const app = express();
-let port = 3000;
 
-const bodyParser = require('body-parser')
-app.use(express.static('public'));
-app.use(bodyParser.json());
+import RelatedView from './relatedView.jsx';
+const {relatedArray} = require('./fakedata.js');
 
-
-//out to heroku
-const relatedProductIds = (id) => {
-  return axios.get(`${url}/products/${id}/related`,{
-    headers: {
-      'Authorization': `token ${config.TOKEN}`
-    }
-  })
-  }
-
-//server listener
-app.get(`product/:product_id/related`, (req,res) => {
-  console.log('we in app.get req', req)
-})
-
-
-class Related extends React.Component {
-  render(){
-    return (
-      <div>some Cards here</div>
-    )
-  }
+const Related = () => {
+  return (
+    <div id="relatedProducts">
+      {relatedArray.map((item, i) => (
+        <RelatedView item={item} key={i}/>
+      ))}
+    </div>
+  )
 }
 
 export default Related
