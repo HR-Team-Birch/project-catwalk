20943adf954c0a762313ed1ef2be7489a3f28b67
Amy Shi <amyshi218@gmail.com>
2022-02-14 17:10:03 -0500

    renamed css file to reviews.css

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './component/app.jsx';
-import "../public/styles.css";
+import "../public/reviews.css";
 
 
 ReactDOM.render(<App />, document.getElementById('app'));
