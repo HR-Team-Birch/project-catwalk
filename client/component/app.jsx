a8b3b02dc8a802b465006fe5cf78ac58613f1bbc
Jennifer Giri <jenniferanngiri@gmail.com>
2022-02-17 12:49:18 -0500

    merged

--- client/component/app.jsx
+++ client/component/app.jsx
@@ -14,7 +14,6 @@ const App = () => {
   const getProducts = () => {
     axios.get(`${url}/products`)
       .then((result) => {
-        //console.log('results', result)
         setProducts(result.data);
         setSelectedProduct(result.data[0]);
         setProductIdforQuestions(result.data[0].id)
@@ -25,7 +24,6 @@ const App = () => {
 
   useEffect(() => {
     getProducts();
-    //console.log('products: ', products)
   }, []);
 
   return (
@@ -37,36 +35,4 @@ const App = () => {
     </div>
   );
 }
-
-
-//import React from 'react';
-//import Overview from './widgets/overview/overview.jsx';
-//import Reviews from './widgets/reviews/reviews.jsx';
-// import Questions from './widgets/questions/questions.jsx';
-//import Questions from './widgets/questions/questions.jsx';
-//import Questions from './widgets/questions/questions.jsx';
-// import RelatedComparison from './widgets/related/relatedCompare.jsx';
-
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       products: []
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <Overview />
-//         <RelatedComparison/>
-//         <Questions />
-//         <Reviews />
-
-//       </div>
-//     )
-//   }
-// }
-
 export default App;
