244dcfd2457ad10e33a68ad00de426095a960b34
Jennifer Giri <jenniferanngiri@gmail.com>
2022-02-20 11:13:20 -0500

    getting hook version of app.jsx to work

--- client/component/app.jsx
+++ client/component/app.jsx
@@ -1,68 +1,43 @@
-// import React, { useEffect, useState } from 'react';
-// // import Overview from './widgets/overview/overview.jsx';
-// // import Reviews from './widgets/reviews/reviews.jsx';
-// import Questions from './widgets/questions/questions.jsx';
-// import RelatedComparison from './widgets/related/relatedCompare.jsx';
-// const axios = require('axios');
-// const url = 'http://localhost:3000';
-
-// const App = () => {
-//   const [products, setProducts] = useState([]);
-//   const [selectedProduct, setSelectedProduct] = useState(null);
-//   const [productIdforQuestions, setProductIdforQuestions] = useState('')
-
-//   const getProducts = () => {
-//     axios.get(`${url}/products`)
-//       .then((result) => {
-//         //console.log('results', result)
-//         setProducts(result.data);
-//         setSelectedProduct(result.data[0]);
-//         setProductIdforQuestions(result.data[0].id)
-//       }).catch((error) => {
-//         console.log('Error: ', error);
-//       });
-//   };
-
-//   useEffect(() => {
-//     getProducts();
-//     //console.log('products: ', products)
-//   }, []);
-
-
-//   return (
-//     <div>
-//       { // TODO pass state function to the overview to change the selected product
-//         /* <Overview setSelectedProduct={setSelectedProduct}/>
-//         <Reviews /> */}
-//       {/* <RelatedComparison /> */}
-//       <Questions productId={productIdforQuestions} />
-//     </div>
-//   );
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Overview from './widgets/overview/overview.jsx';
-import Reviews from './widgets/reviews/reviews.jsx';
-import Questions from './widgets/questions/questions.jsx';
-// import RelatedComparison from './widgets/related/relatedCompare.jsx';
+// import Reviews from './widgets/reviews/reviews.jsx';
+//import Questions from './widgets/questions/questions.jsx';
+//import RelatedComparison from './widgets/related/relatedCompare.jsx';
+import axios from 'axios';
+const url = 'http://localhost:3000';
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [productIdforQuestions, setProductIdforQuestions] = useState('')
+
+  const getProducts = () => {
+    axios.get(`${url}/products`)
+      .then((result) => {
+        //console.log('results', result)
+        setProducts(result.data);
+        setSelectedProduct(result.data[0]);
+        setProductIdforQuestions(result.data[0].id)
+      }).catch((error) => {
+        console.log('Error: ', error);
+      });
+  };
+
+  useEffect(() => {
+    getProducts();
+    //console.log('products: ', products)
+  }, []);
 
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: []
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <Overview />
-        {/*<Reviews />
-        <Questions />*/}
-
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Overview />
+      {/*{   <Reviews />}*/}
+      {/* <RelatedComparison /> */}
+      {/*<Questions productId={productIdforQuestions} />
+      */}
+    </div>
+  );
 }
 
 export default App;
