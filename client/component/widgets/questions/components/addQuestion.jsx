8a75c4966c0a2004b3a03674f1bd3ba2a291a0fb
BJ White <bjwhite0512@gmail.com>
2022-02-20 23:15:30 -0500

    add an answer modal is working correctly

--- client/component/widgets/questions/components/addQuestion.jsx
+++ client/component/widgets/questions/components/addQuestion.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import AddQuestionModal from './AddQuestionModal.jsx';
 
 const AddQuestion = ({ showAddQuestionModal }) => {
 
   return (
     <div id="addQuestion">
-      <button id="addQButton" onClick={showAddQuestionModal}>ADD A QUESTION +</button>
+      <button id="addQButton" type="button" onClick={showAddQuestionModal}>{`ADD A QUESTION   +`}</button>
     </div>
   )
 }
