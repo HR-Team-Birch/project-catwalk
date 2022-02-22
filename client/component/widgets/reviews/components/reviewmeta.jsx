947896e9e4c3966deab6c23e80d066eea6ca3ec1
Amy Shi <amyshi218@gmail.com>
2022-02-22 16:27:04 -0500

    rendering characteristics bar triangle

--- client/component/widgets/reviews/components/reviewmeta.jsx
+++ client/component/widgets/reviews/components/reviewmeta.jsx
@@ -24,6 +24,20 @@ const ReviewMeta = ({reviewMeta, reviews}) => {
   const [twoStarPercentage, setTwoStarPercentage] = useState(0);
   const [oneStarPercentage, setOneStarPercentage] = useState(0);
 
+  const [fitBar, setFitBar] = useState(0);
+  const [sizeBar, setSizeBar] = useState(0);
+  const [comfortBar, setComfortBar] = useState(0);
+  const [qualityBar, setQualityBar] = useState(0);
+  const [widthBar, setWidthBar] = useState(0);
+  const [lengthBar, setLengthBar] = useState(0);
+
+  const [showFitBar, setShowFitBar] = useState(false);
+  const [showSizeBar, setShowSizeBar] = useState(false);
+  const [showComfortBar, setShowComfortBar] = useState(false);
+  const [showQualityBar, setShowQualityBar] = useState(false);
+  const [showWidthBar, setShowWidthBar] = useState(false);
+  const [showLengthBar, setShowLengthBar] = useState(false);
+
   const starBarsPercentage = () => {
     let totalReviews = reviews.length;
     setFiveStarPercentage(Math.round((fiveStarCount/totalReviews) * 100));
@@ -75,6 +89,39 @@ const ReviewMeta = ({reviewMeta, reviews}) => {
     setOneStarCount(one)
   };
 
+  const characteristicsTriangle = () => {
+
+    if (reviewMeta.characteristics.Fit) {
+      setShowFitBar(true);
+      setFitBar(Math.round((reviewMeta.characteristics.Fit.value / 5) * 100))
+    }
+    if (reviewMeta.characteristics.Size) {
+      setShowSizeBar(true);
+      setSizeBar(Math.round((reviewMeta.characteristics.Size.value / 5) * 100))
+    }
+
+    if (reviewMeta.characteristics.Comfort) {
+      setShowComfortBar(true);
+      setComfortBar(Math.round((reviewMeta.characteristics.Comfort.value / 5) * 100))
+    }
+
+    if (reviewMeta.characteristics.Quality) {
+      setShowQualityBar(true);
+      setQualityBar(Math.round((reviewMeta.characteristics.Quality.value / 5) * 100))
+    }
+
+    if (reviewMeta.characteristics.Width) {
+      setShowWidthBar(true);
+      setWidthBar(Math.round((reviewMeta.characteristics.Width.value / 5) * 100))
+    }
+
+    if (reviewMeta.characteristics.Length) {
+      setShowLengthBar(true);
+      setLengthBar(Math.round((reviewMeta.characteristics.Length.value / 5) * 100))
+    }
+
+  }
+
   // const starFilterClicked = () => {
 
   // }
@@ -85,6 +132,7 @@ const ReviewMeta = ({reviewMeta, reviews}) => {
     getPercentRecommend();
     countReviews()
     starBarsPercentage()
+    characteristicsTriangle()
   }, [])
 
   useEffect(() => {
@@ -180,83 +228,77 @@ const ReviewMeta = ({reviewMeta, reviews}) => {
 
       <div id="characteristicscontainer">
 
+        {showFitBar ?
         <div className="characteristics">
-          <div id="fit" style={{fontSize: "14px"}}>Fit</div>
+          <div id="charname" style={{fontSize: "14px"}}>Fit</div>
           <div className="charparent">
             <div className="charcontainer">
-              <div className="fit" style={{width: `${fiveStarPercentage}%`}}>{triangle}</div>
+              <div className="fit" style={{width: `${fitBar}`}}>{triangle}</div>
             </div>
           </div>
-          <div className="starsBarsRight">
-            <u style={{fontSize: "14px"}}  >{fiveStarCount}</u>
-          </div>
-          {/* {TODO make the triangle to show the percentage} */}
         </div>
+        : null
+        }
 
+        {showSizeBar ?
         <div className="characteristics">
-          <div id="fit" style={{fontSize: "14px"}}>Size</div>
+          <div id="charname" style={{fontSize: "14px"}}>Size</div>
           <div className="charparent">
             <div className="charcontainer">
-              <div className="sizechar" style={{width: `${fiveStarPercentage}%`}}></div>
+              <div className="sizechar" style={{width: `${sizeBar}%`}}>{triangle}</div>
             </div>
           </div>
-          <div className="starsBarsRight">
-            <u style={{fontSize: "14px"}}  >{fiveStarCount}</u>
-          </div>
-          {/* {TODO make the triangle to show the percentage} */}
         </div>
+        : null
+        }
 
+        {showComfortBar ?
         <div className="characteristics">
-          <div id="fit" style={{fontSize: "14px"}}>Comfort</div>
+          <div id="charname" style={{fontSize: "14px"}}>Comfort</div>
           <div className="charparent">
             <div className="charcontainer">
-              <div className="comfort" style={{width: `${fiveStarPercentage}%`}}></div>
+              <div className="comfort" style={{width: `${comfortBar}%`}}>{triangle}</div>
             </div>
           </div>
-          <div className="starsBarsRight">
-            <u style={{fontSize: "14px"}}  >{fiveStarCount}</u>
-          </div>
-          {/* {TODO make the triangle to show the percentage} */}
         </div>
+        : null
+        }
 
+        {showQualityBar ?
         <div className="characteristics">
-          <div id="fit" style={{fontSize: "14px"}}>Quality</div>
+          <div id="charname" style={{fontSize: "14px"}}>Quality</div>
           <div className="charparent">
             <div className="charcontainer">
-              <div className="quality" style={{width: `${fiveStarPercentage}%`}}></div>
+              <div className="quality" style={{width: `${qualityBar}%`}}>{triangle}</div>
             </div>
           </div>
-          <div className="starsBarsRight">
-            <u style={{fontSize: "14px"}} >{fiveStarCount}</u>
-          </div>
-          {/* {TODO make the triangle to show the percentage} */}
         </div>
+        : null
+        }
 
+        {showWidthBar ?
         <div className="characteristics">
-          <div id="fit" style={{fontSize: "14px"}}>Width</div>
+          <div id="charname" style={{fontSize: "14px"}}>Width</div>
           <div className="charparent">
             <div className="charcontainer">
-              <div className="width" style={{width: `${fiveStarPercentage}%`}}></div>
+              <div className="width" style={{width: `${widthBar}%`}}>{triangle}</div>
             </div>
           </div>
-          <div className="starsBarsRight">
-            <u style={{fontSize: "14px"}} >{fiveStarCount}</u>
-          </div>
-          {/* {TODO make the triangle to show the percentage} */}
         </div>
+        : null
+        }
 
+        {showLengthBar ?
         <div className="characteristics">
-          <div id="fit" style={{fontSize: "14px"}}>Length</div>
+          <div id="charname" style={{fontSize: "14px"}}>Length</div>
           <div className="charparent">
             <div className="charcontainer">
-              <div className="length" style={{width: `${fiveStarPercentage}%`}}></div>
+              <div className="length" style={{width: `${lengthBar}%`}}>{triangle}</div>
             </div>
           </div>
-          <div className="starsBarsRight">
-            <u style={{fontSize: "14px"}} >{fiveStarCount}</u>
-          </div>
-          {/* {TODO make the triangle to show the percentage} */}
         </div>
+        : null
+        }
 
 
 
