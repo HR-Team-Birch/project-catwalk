ce6d5189a93e8446489c7b473ff418d523ecffaf
Jennifer Giri <jenniferanngiri@gmail.com>
2022-02-24 11:20:01 -0500

    pre-merge

--- client/component/app.jsx
+++ client/component/app.jsx
@@ -73,7 +73,8 @@ const App = () => {
 
   return (
     <div>
-      <Overview reviewMeta={reviewMeta} selectedProduct={selectedProduct} currentStyle={currentStyle} setCurrentStyle={setCurrentStyle} allStyles={allStyles} setAllStyles={setAllStyles} productFeatures={productFeatures}/>
+      {selectedProduct && 
+        <Overview reviewMeta={reviewMeta} selectedProduct={selectedProduct} currentStyle={currentStyle} setCurrentStyle={setCurrentStyle} allStyles={allStyles} setAllStyles={setAllStyles} productFeatures={productFeatures}/>}
       <RelatedComparison/>
       <Questions productId={productIdforQuestions} product={selectedProduct}/>
       {productIdforQuestions && reviewMeta ?  <Reviews productId={productIdforQuestions} product={selectedProduct.name} reviewMeta={reviewMeta}/> : null }
