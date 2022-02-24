57a919a1e5f458c72cbb2652a249ad0dc4de87d8
BJ White <bjwhite0512@gmail.com>
2022-02-24 09:07:19 -0500

    questions widget

---
+++ client/component/widgets/questions/components/makeTitle.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MakeTitle = ({titleText, filteredStatus}) => {
+  console.log('titleText', titleText);
+  return (
+    <div className='question'> Q: {titleText}</div>
+  );
+}
+
+export default MakeTitle;
